30268150f159bf956a38245555c67b63eaf13d0d
Name?= null <Ire.Raufu76@gmail.com>
2020-12-04 18:21:46 +0000

    Add error handling, Add CLI

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo -n "Enter snippet name >> "
-read name
+echo -n "Enter snippet name >> "; read name
 
-python "snippets/$name.py"
+if python "snippets/$name.py"; then
+    printf "\n\nSnippet '$name' run successfully.\n"
+else
+    printf "\n\nAn error occured while running '$name'\n"
+    exit 1
+fi
