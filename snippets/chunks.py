8729be50aba1154415d1180cd4c9662ffb8a1694
Name?= null <Ire.Raufu76@gmail.com>
2020-12-08 15:37:11 +0000

    Add chunks() and gen_chunks() snippet

---
+++ snippets/chunks.py
@@ -0,0 +1,19 @@
+import pprint
+
+pp = pprint.PrettyPrinter(indent=2)
+
+def chunks(lst: list, size: int):
+    return [lst[i:i + size] for i in range(0, len(lst), size)]
+
+def gen_chunks(lst: list, size: int):
+    for i in range(0, len(lst), size):
+        yield lst[i:i + size]
+
+l = [i for i in range(25)]
+
+print("Split into chunks in one go!")
+pp.pprint(chunks(l, 10))
+
+print("Loop through each chuck!")
+for chunk in gen_chunks(l, 10):
+    pp.pprint(chunk)
